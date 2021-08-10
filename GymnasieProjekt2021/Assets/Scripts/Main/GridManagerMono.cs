fb7a77db44766a890b7ff137895b4bfd595ce643
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2021-08-10 12:51:17 +0200

    fix

--- GymnasieProjekt2021/Assets/Scripts/Main/GridManagerMono.cs
+++ GymnasieProjekt2021/Assets/Scripts/Main/GridManagerMono.cs
@@ -2,29 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridManagerMono : MonoBehaviour
-{
-    public Transform player;
+public class GridManagerMono : MonoBehaviour {
     public int gridLength = 8, gridHeight = 8, chunkLength = 3;
-    private void Awake(){
-        GridManager.InitializeChunks(chunkLength, gridLength, gridHeight);
-    }
-    /*
-    private void OnDrawGizmos(){
-        Gizmos.color = Color.red;
-        for(int i = 0; i < GridManager.chunks.Count; i++){
-            Gizmos.DrawWireCube(GridManager.chunks[i].position, GridManager.chunks[i].size);
-        }
+    private void Awake() {
+        GridManager.chunkLength = chunkLength;
+        GridManager.gridLength = gridLength;
+        GridManager.gridHeight = gridHeight;
 
-        Gizmos.color = Color.white;
-        if (Application.isPlaying)
-        {
-            Gizmos.DrawWireCube(GridManager.GetChunk(player.position).position, GridManager.GetChunk(player.position).size);
-            for (int i = 0; i < GridManager.GetChunk(player.position).cells.Count; i++)
-            {
-                Gizmos.DrawWireCube(GridManager.GetChunk(player.position).cells[i].position, Vector3.one);
+        GridManager.InitializeChunks(chunkLength, gridLength, gridHeight);
+
+        GridManager.LoadChunk(GridManager.GetChunk(GameManager.player.transform.position));
+    }
+    private void FixedUpdate(){
+        GridManager.LoadChunks(GridManager.GetSorroundingChunks(GameManager.player.transform.position));
+    }
+    private void OnDrawGizmos() {
+        if (Application.isPlaying){
+            Gizmos.color = Color.red;
+
+            for (int i = 0; i < GridManager.chunks.Count; i++){
+                if (GridManager.chunks[i].isLoaded)
+                    Gizmos.color = Color.yellow;
+                else if (!GridManager.chunks[i].isLoaded)
+                    Gizmos.color = Color.red;
+                Gizmos.DrawWireCube(GridManager.chunks[i].position, GridManager.chunks[i].size);
             }
+
+            Gizmos.color = Color.green;
+            List<GridManager.Chunk> sorroundingChunks = GridManager.GetSorroundingChunks(GameManager.player.transform.position); 
+            for (int i = 0; i < sorroundingChunks.Count; i++){
+                Gizmos.DrawWireCube(sorroundingChunks[i].position, sorroundingChunks[i].size);
+            }
+
+            /*
+            GridManager.Chunk[] chunks = GridManager.GetSorroundingChunks(GameManager.player.transform.position);
+            for (int i = 0; i < chunks.Length; i++)
+            {
+                Gizmos.DrawWireCube(chunks[i].position, chunks[i].size);
+            }
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(GridManager.GetChunk(GameManager.player.transform.position).position, GridManager.GetChunk(GameManager.player.transform.position).size);
+            */
         }
     }
-    */
 }
+
