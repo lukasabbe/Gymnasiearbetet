d6ae98f88225bfb6319c31a53c6093d313706755
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2021-08-10 12:47:01 +0200

    AI

---
+++ GymnasieProjekt2021/Assets/Editor/AiInspctorMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AIManeger))]
+
+public class AiInspctorMenu : Editor
+{
+    private int selected = 0;
+
+    string[] options = new string[] { "Shooting", "Melee" };
+    public override void OnInspectorGUI()
+    {
+
+        GUILayout.Label("Setings");
+        base.OnInspectorGUI();
+        EditorGUI.BeginChangeCheck();
+        this.selected = EditorGUILayout.Popup("AI type", selected, options);
+        if (EditorGUI.EndChangeCheck())
+        {
+            
+        }
+    }
+}
