c5e8a2633d51d79190a4707f890769eecd2cd3ec
bohlenius <78263552+bohlenius@users.noreply.github.com>
2021-09-10 14:20:19 +0200

    npc

---
+++ GymnasieProjekt2021/Assets/AI/Script/NPCBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCBase : MonoBehaviour
+{
+    private INPCState currentState;
+    public WanderStateBase wanderState;
+    public AttackState attackState = new AttackState();
+
+    [HideInInspector] public NavMeshAgent navAgent;
+
+    [HideInInspector] public Vector3 spawnPoint;
+    
+    private void OnEnable() {
+        navAgent = GetComponent<NavMeshAgent>();
+
+        spawnPoint = transform.position;
+
+        currentState = wanderState;
+    }
+    private void Update() {
+        currentState = currentState.DoState(this);
+    }
+}
